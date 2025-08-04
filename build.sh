0980aaf13a31ff0615050f641bd6490231134e6c
Sumaya Hashim <64749377+SpunkySam-cyber@users.noreply.github.com>
2025-08-04 11:48:55 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+
+# Bring pip/build tooling up to date so packages can generate metadata
+python3 -m pip install --upgrade pip setuptools wheel
 
 # Install Python dependencies
 pip3 install -r python-requirements.txt
 
-# Build the application
+# Build the application (frontend + bundle)
 npm run build
